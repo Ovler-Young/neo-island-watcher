ad1cb28180b228a935ef218be49c542c6920a38e
Ovler <ovlertheyoung@gmail.com>
2025-09-29 06:00:42 -0400

    Remove debug logging from thread ID error messages in PO commands

--- src/bot/commands/po.ts
+++ src/bot/commands/po.ts
@@ -21,8 +21,7 @@ export function setupPoCommands(bot: Bot) {
 			if (!threadId) {
 				await ctx.reply(
 					"❌ Unable to determine thread ID.\n" +
-						"This command should be used in a thread topic.\n" +
-						JSON.stringify(ctx),
+						"This command should be used in a thread topic.\n"
 				);
 				return;
 			}
@@ -65,8 +64,7 @@ export function setupPoCommands(bot: Bot) {
 			if (!threadId) {
 				await ctx.reply(
 					"❌ Unable to determine thread ID.\n" +
-						"This command should be used in a thread topic.\n" +
-						JSON.stringify(ctx),
+						"This command should be used in a thread topic.\n"
 				);
 				return;
 			}
